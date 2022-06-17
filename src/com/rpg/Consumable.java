f8f64642d2596d5b048292f364a9d76454d021bb
Muhammed <91959901+itsmomo300@users.noreply.github.com>
2022-06-17 10:35:31 +0200

    Delete src directory

--- src/com/rpg/Consumable.java
+++
@@ -1,5 +0,0 @@
-package com.rpg;
-
-public interface Consumable {
-   public int getLifePoints();
-}
--- src/com/rpg/Food.java
+++
@@ -1,8 +0,0 @@
-package com.rpg;
-
-public class Food implements Consumable {
-
-    public int getLifePoints(){
-        return 10;
-    }
-}
